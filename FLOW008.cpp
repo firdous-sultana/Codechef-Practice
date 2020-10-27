bda18d89e3699884d3f3bd04200ebc6d97559fca
firdous-sultana <firdoussultana54@gmail.com>
2020-10-27 18:35:17 +0530

    Helping Chef

---
+++ FLOW008.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int test;
+	cin>>test;
+	while(test--){
+		int n;
+		cin>>n;
+		(n>= 10) ? cout<<"-1\n" : cout<<"Thanks for helping Chef!\n";		
+	}
+	return 0;
+}
