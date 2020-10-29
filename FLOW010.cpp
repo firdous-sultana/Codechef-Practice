8a32f4ff385a8a5a804ed077b2935d3f17ae3477
firdous-sultana <firdoussultana54@gmail.com>
2020-10-29 15:36:34 +0530

    Id and Ship

---
+++ FLOW010.cpp
@@ -0,0 +1,26 @@
+//Id and Ship
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		char ch;
+		cin>>ch;
+		switch(ch){
+			case 'B':
+			case 'b': cout<<"BattleShip\n";
+					  break;
+			case 'C':
+			case 'c': cout<<"Cruiser\n";
+					  break;
+			case 'D':
+			case 'd': cout<<"Destroyer\n";
+					  break;
+			case 'F':
+			case 'f': cout<<"Frigate\n";
+					  break;
+		}
+	}
+	return 0;
+}
