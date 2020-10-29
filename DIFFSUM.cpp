66a3a805002a2e0be6b061c5c637104732f3f758
firdous-sultana <firdoussultana54@gmail.com>
2020-10-29 14:51:54 +0530

    Sum OR Difference

---
+++ DIFFSUM.cpp
@@ -0,0 +1,9 @@
+//Sum OR Difference
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n1, n2;
+	cin>>n1>>n2;
+	(n1 > n2) ? cout<<(n1 - n2)<<endl : cout<<(n1+n2)<<endl;
+	return 0;
+}
